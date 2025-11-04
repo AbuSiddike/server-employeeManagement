b3f6079c8b9ff375684d5e123de378321183be91
AbuSiddike <abubakkarsiddike1997@gmail.com>
2025-11-05 04:36:52 +0600

    employee management server in progress

---
+++ db/connect.js
@@ -0,0 +1,30 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+let db;
+
+export async function connectDB() {
+    try {
+        await client.connect();
+        db = client.db("employeeManagement");
+        console.log("MongoDB successfully connected !");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+export function getDB() {
+    if (!db) throw new Error("Database not connected yet!");
+    return db;
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connect.js";
+
+// Routes
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+    res.send("Employee Management Server is running");
+});
+
+// Register Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/work-records", workRoutes);
+// app.use("/api/payrolls", payrollRoutes);
+// app.use("/api/contact-messages", contactRoutes);
+
+// Connect to Database and start Server
+const port = process.env.PORT || 5000;
+
+connectDB().then(() => {
+    app.listen(port, () => console.log(`Server is running on port: ${port}`));
+});
